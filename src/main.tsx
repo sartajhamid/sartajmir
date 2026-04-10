d6272c81cf90aef181d9d63c40b18b6303e8cdfb
Sartaj Ahmad mir <105062126+sartajhamid@users.noreply.github.com>
2026-04-10 15:48:37 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+const redirect = new URLSearchParams(window.location.search).get('redirect');
+if (redirect) {
+  window.history.replaceState(null, '', redirect);
+}
